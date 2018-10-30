13f578009031da03147982e7d4ac826c246fb4c1
EneloopAA <41654945+EneloopAA@users.noreply.github.com>
2018-10-30 18:00:30 +0900

    Add files.
    First file uploaded.
    
    step one for logging.

---
+++ main.py
@@ -0,0 +1,17 @@
+import PyQt5
+import time
+
+log_dict = {'high_start': '고온챔버 동작을 시작합니다.',
+            'high_ready':'고온챔버 준비를 시작합니다.',
+            'high_end':'고온챔버 동작이 종료되었습니다.',
+            'low_free_interlock':'상온챔버 인터락 해제되었습니다.',
+            'low_start': '상온챔버 동작을 시작합니다.'}
+
+
+def log(action):
+    time_dic = time.localtime(time.time())
+    log_message = '[ {}:{}:{} ] {}'.format(time_dic.tm_hour, time_dic.tm_min, time_dic.tm_sec, log_dict[action])
+    print(log_message, end='')
+
+
+log('high_start')
