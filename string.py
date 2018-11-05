de0e9740de5d594a0548fb1c2a470bce46d60631
EneloopAA <41654945+EneloopAA@users.noreply.github.com>
2018-11-05 10:49:34 +0900

    Create string.py

---
+++ string.py
@@ -0,0 +1,5 @@
+log_dict = {'high_start': '고온챔버 동작을 시작합니다.',
+            'high_ready': '고온챔버 준비를 시작합니다.',
+            'high_end': '고온챔버 동작이 종료되었습니다.',
+            'low_free_interlock': '상온챔버 인터락 해제되었습니다.',
+            'low_start': '상온챔버 동작을 시작합니다.'}
